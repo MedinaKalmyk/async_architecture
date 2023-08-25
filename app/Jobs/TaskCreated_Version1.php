516e0e58dd0d0e125587f88ab8122a2cd4611727
medinakalmyk <kalmykme@gmail.com>
2023-08-25 10:51:12 +0300

    Homework4

--- app/Jobs/TaskCreated_Version1.php
+++ app/Jobs/TaskCreated_Version1.php
@@ -29,59 +29,65 @@ class TaskCreated_Version1 implements ShouldQueue
      */
     public function handle() {
 
-        $conf = new Conf();
-        $conf->set('group.id', 'mygroup');
-        $conf->set("bootstrap.servers", 'kafka:19092');
-        $conf->set('metadata.broker.list', 'kafka:19092');
-        $conf->set('auto.commit.interval.ms', 9999999);
-        $conf->set('auto.offset.reset', 'largest');
-//        $conf->set("auto.commit.interval.ms", 1e3);
-//        $conf->set('enable.auto.commit', 'true');
+        $conf = new \RdKafka\Conf();
 
-        $consumer = new KafkaConsumer($conf);
+        $conf->setRebalanceCb(function (\RdKafka\KafkaConsumer $kafka, $err, array $partitions = null) {
+            switch ($err) {
+                case RD_KAFKA_RESP_ERR__ASSIGN_PARTITIONS:
+                    echo "Assign: ";
+                    var_dump($partitions);
+                    $kafka->assign($partitions);
+                break;
+
+                case RD_KAFKA_RESP_ERR__REVOKE_PARTITIONS:
+                    echo "Revoke: ";
+                    var_dump($partitions);
+                    $kafka->assign(NULL);
+                break;
+
+                default:
+                    throw new \Exception($err);
+            }
+        });
+
+        $conf->set('group.id', 'mygroup');
+
+        $conf->set('metadata.broker.list', 'kafka:19092');
+
+        $conf->set('auto.offset.reset', 'earliest');
+
+        $consumer = new \RdKafka\KafkaConsumer($conf);
+
         $consumer->subscribe(['TaskLifeStyleVersion2']);
 
-//        $message = $consumer->consume(2000);
-//
-//        dd($message);
+        echo "Waiting for partition assignment... (make take some time when\n";
+        echo "quickly re-joining the group after leaving it.)\n";
 
-//        $topic = $consumer->newTopic("TaskLifeStyleVersion2");
-
-
-      //  while(true) {
-            $message = $consumer->consume(20000);
-
-            switch($message->err) {
-
-                case RD_KAFKA_RESP_ERR_NO_ERROR:
-
-                    $json = json_decode($message->payload);
-
-                    DB::table('task')
-                        ->insert([
-                            'name' => $json->name,
-                            'description' => $json->description,
-                            'price' => $json->price,
-                            'userId' => $json->userId,
-                            'title' => $json->name,
-                            'jira_id' => $json->jira_id,
-                        ]);
-                    // }
-                    $consumer->commit();
-//                    $consumer->close();
-                    //  dd($topic);
-                break;
-                case RD_KAFKA_RESP_ERR__PARTITION_EOF:
-                    $consumer->close();
-                    // Не ошибка, просто достигнут конец очереди
-                break;
-                default:
-                    error_log("Error consuming message TaskCreated2: " . $message->errstr() . "\n");
-
-                    // Здесь можно добавить обработку ошибок, например, запись в лог
-                    //   echo "Error consuming message: " . $message->errstr() . "\n";
-                break;
-            }
+        $message = $consumer->consume(2000);
+        switch ($message->err) {
+            case RD_KAFKA_RESP_ERR_NO_ERROR:
+                $json = json_decode($message->payload);
+                DB::table('task')
+                    ->insert([
+                        'name' => $json->name,
+                        'description' => $json->description,
+                        'price' => $json->price,
+                        'userId' => $json->userId,
+                        'title' => $json->name,
+                        'jira_id' => $json->jira_id,
+                    ]);
+                $consumer->commit($message);
+            break;
+            case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+                echo "No more messages; will wait for more\n";
+            break;
+            case RD_KAFKA_RESP_ERR__TIMED_OUT:
+                echo "Timed out\n";
+            break;
+            default:
+                throw new \Exception($message->errstr(), $message->err);
+            break;
         }
-   // }
+    }
+
 }
